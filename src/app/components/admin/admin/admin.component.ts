94caea912c017a43917d81ce3c21a5946480914e
Manoj Kapure <manojk@bbd.co.za>
2023-05-11 14:29:53 +0530

    chart added

--- src/app/components/admin/admin/admin.component.ts
+++ src/app/components/admin/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ChartData } from 'chart.js';
 
 @Component({
   selector: 'app-admin',
@@ -6,5 +7,49 @@ import { Component } from '@angular/core';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent {
+
+  chartDataarr: ChartData[] = [
+    {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [
+        {
+          data: [65, 59, 80, 81, 56, 55, 40],
+          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#E7E9ED', '#4BC0C0', '#FF8F80', '#A9A9FF'],
+          borderColor: ['#FF6384', '#36A2EB', '#FFCE56', '#E7E9ED', '#4BC0C0', '#FF8F80', '#A9A9FF'],
+          borderWidth: 1
+        }
+      ]
+    },
+    {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [
+        {
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF', '#FF8F80'],
+          borderColor: ['#FF6384', '#36A2EB', '#FFCE56', '#4BC0C0', '#9966FF', '#FF8F80'],
+          borderWidth: 1
+        }
+      ]
+    }
+  ];
+  
+
+
+  chartData = {
+    labels: ['Red', 'Pink', 'Green', 'Yellow', 'Orange', 'Blue',],
+    datasets: [{
+      label: 'My First Dataset',
+      data: [150, 150, 150, 150, 150, 150],
+      backgroundColor: [
+        'red',
+        'pink',
+        'green',
+        'yellow',
+        'orange',
+        'blue'
+      ],
+      hoverOffset: 4
+    }],
+  }
 
 }
--- src/app/components/admin/dashboard/dashboard.component.ts
+++ src/app/components/admin/dashboard/dashboard.component.ts
@@ -1,29 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import Chart, { ChartData } from 'chart.js/auto';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
-  items=[
+  items = [
     {
-      id:1,
-      name:'manoj',
-      status:'active',
-      action:'add'
+      id: 1,
+      name: 'manoj',
+      status: 'active',
+      action: 'add'
     },
     {
-      id:2,
-      name:'dsds',
-      status:'block',
-      action:'remove'
+      id: 2,
+      name: 'dsds',
+      status: 'block',
+      action: 'remove'
     }
   ];
 
-  constructor(){
-      
+  @Input() data!: ChartData;
+  @Input() type!: string;
+
+  chart: any;
+
+  constructor() {
+  }
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+    this.createChart(this.data,this.type);
+  }
+
+  createChart(data: ChartData, type:String) {
+
+    this.chart = new Chart("MyChart", {
+      type: 'doughnut', //this denotes tha type of chart
+
+      data: this.data,
+      options: {
+        aspectRatio: 2.5
+      }
+
+    });
   }
 
 }
